55d937473989e4e1b91c6f60d66406b4efa31296
zpoint <zp@zp0int.com>
2017-02-05 00:15:18 +0800

    win32 release version

--- multithreading_ping/p.cpp
+++ multithreading_ping/p.cpp
@@ -1,110 +1,136 @@
-#include <stdlib.h>     /* system, NULL, EXIT_FAILURE */
-#include <stdio.h>  /* popen */
-#include <stdlib.h>
-#include <string>
-#include <iostream>
-#include <vector>
 #include <algorithm>
 #include "thread.h"
 #define MAXLINE 1024
 #define MAX_LINE 5  // ping up to 5 lines to generate result 
+#ifdef _WIN32
+#define PING_COMMAND std::string( "ping")
+
+double get_avg(const std::string line)
+{
+	// std::cout << line << std::endl;
+	std::size_t pos = line.find("Average ="), pos_end;
+	if (pos == std::string::npos) // fail in findling average
+		return -1;
+
+	std::string sub_line = line.substr(pos + 1);
+	// find =
+	pos = sub_line.find_first_of("=") + 2;
+	pos_end = sub_line.find_first_of("m");
+	sub_line = sub_line.substr(pos, pos_end - pos);
+	return static_cast<double>(atof(sub_line.c_str()));
+}
+
+# else
+#define PING_COMMAND std::string( "ping -c ") + std::to_string(MAX_LINE)
+double get_avg(const std::string line)
+{
+	// std::cout << line << std::endl;
+	if (line.find("min/avg/max/mdev") == std::string::npos) // fail to find correct ping result
+		return -1;
+	// find '='
+	std::size_t pos = line.find_first_of("=");
+	std::string sub_line = line.substr(pos + 1);
+	// find max
+	pos = sub_line.find_first_of("/");
+	sub_line = sub_line.substr(pos + 1);
+	// find avg
+	sub_line = sub_line.substr(0, sub_line.find_first_of("/"));
+	return static_cast<double>(atof(sub_line.c_str()));
+}
+#endif
 
 
 bool pre_check_sys()
 {
-		std::cout << "Checking if processor is available..." << std::endl;
-		if (system(NULL)) 
-		{
-				std::cout << "Ok" << std::endl;
-				return true;
-		}
-		else 
-		{
-				std::cout << "cannot run system command " << std::endl;
-				return false;
-		}
+	std::cout << "Checking if processor is available..." << std::endl;
+	if (system(NULL))
+	{
+		std::cout << "Ok" << std::endl;
+		return true;
+	}
+	else
+	{
+		std::cout << "cannot run system command " << std::endl;
+		return false;
+	}
 }
 
 std::string ping(const std::string ip_addr)
 {
-		char line[MAXLINE];
-		std::string last_line;
-		FILE *fpin;
-		std::vector<std::string> vec_lines;
-		std::string command = "ping -c " + std::to_string(MAX_LINE) + " " + ip_addr + " 2>&1";  // static cast
-		// std::cout << "Executing command " << command << "\r\n";
-		if ((fpin = popen(command.c_str(), "r")) == NULL)
-		{
-				std::cerr << "Popen error, command: " << command << std::endl;
-				exit (EXIT_FAILURE);
-		}
-		while (fgets(line, MAXLINE, fpin) != NULL)
-				last_line = line;
-		if (pclose(fpin) == -1)
-				std::cerr << "pclose error for command: " << command << std::endl;
-		return last_line;
+	char line[MAXLINE];
+	std::string last_line;
+	FILE *fpin;
+	std::vector<std::string> vec_lines;
+	std::string command = PING_COMMAND + " " + ip_addr + " 2>&1";  // static cast
+	// std::cout << "Executing command " << command << "\r\n";
+	if ((fpin = POPEN(command.c_str(), "r")) == NULL)
+	{
+		std::cerr << "Popen error, command: " << command << std::endl;
+		exit(EXIT_FAILURE);
+	}
+	while (fgets(line, MAXLINE, fpin) != NULL)
+		last_line = line;
+	if (PCLOSE(fpin) == -1)
+		std::cerr << "pclose error for command: " << command << std::endl;
+	return last_line;
 }
-
-double get_avg(const std::string line)
-{
-		// std::cout << line << std::endl;
-		if (line.find("min/avg/max/mdev") == std::string::npos) // fail to find correct ping result
-				return -1;
-		// find '='
-		std::size_t pos = line.find_first_of("=");
-		std::string sub_line = line.substr(pos + 1);
-		// find max
-		pos = sub_line.find_first_of("/");
-		sub_line = sub_line.substr(pos + 1);
-		// find avg
-		sub_line = sub_line.substr(0, sub_line.find_first_of("/"));
-		return static_cast<double>(atof(sub_line.c_str()));
-}
-
-
 
 int main(int argc, char **argv)
 {
-		if (argc != 2)
-		{
-				std::cerr << "Usage: program_name filename" << std::endl;
-				exit(EXIT_FAILURE);
-		}
+	if (argc > 2)
+	{
+		std::cerr << "Usage: program_name filename" << std::endl;
+		pause();
+		exit(EXIT_FAILURE);
+	}
 
-		std::ifstream fin(argv[1]);
-		if (!fin.good())
-		{
-				std::cerr << "Unable to open the file: " << argv[1] << std::endl;
-				exit(EXIT_FAILURE);
-		}
+	std::string filename;
+	if (argc == 1)
+	{
+			std::cout << "Please enter a file name which stores ip address line by line: " << std::endl;
+			std::cin >> filename;
+	}
+	else
+			filename = argv[1];
 
-		if (!pre_check_sys())
-		{
-				std::cerr << "Can't use sys command" << std::endl;
-				exit(EXIT_FAILURE);
-		}
-		std::cout << "Executing, please wait..." << std::endl;
-		std::vector<process_struct> results = main_func(fin);
-		if (!results.size())
-		{
-NO_VALID:
-				std::cout << "No valid ip address!!!" << std::endl;
-				exit(EXIT_FAILURE);
-		}
-		std::sort(results.begin(), results.end(), 
-						[] (const process_struct &a, const process_struct &b) { return a.avg_val < b.avg_val; });
-		auto good_item = std::find_if_not(results.begin(), results.end(), [] (const process_struct &a) { return a.avg_val <= 0; });
-		if (good_item == results.end())
-				goto NO_VALID;
-		std::cout << "\r\nSuccess ip address:" << std::endl;
-		std::for_each(good_item, results.end(), 
-						[] (const process_struct &a) { printf("ip address: %-15s, average time: %-6.2f\r\n", a.ip_addr.c_str(), a.avg_val); });
-		if (good_item != results.begin())
-		{
-				std::cout << "\r\nFailure ip address: " << std::endl;
-				std::for_each(results.begin(), good_item,
-								[] (const process_struct &a) { std::cout << "ip address: " << a.ip_addr << std::endl;});
-		}
-		return 0;
+	std::ifstream fin(filename);
+	if (!fin.good())
+	{
+		std::cerr << "Unable to open the file: " << filename << std::endl;
+		pause();
+		exit(EXIT_FAILURE);
+	}
+
+	if (!pre_check_sys())
+	{
+		std::cerr << "Can't use sys command" << std::endl;
+		pause();
+		exit(EXIT_FAILURE);
+	}
+	std::cout << "Executing, please wait..." << std::endl;
+	std::vector<process_struct> results = main_func(fin);
+	if (!results.size())
+	{
+	NO_VALID:
+		std::cout << "No valid ip address!!!" << std::endl;
+		pause();
+		exit(EXIT_FAILURE);
+	}
+	std::sort(results.begin(), results.end(),
+		[](const process_struct &a, const process_struct &b) { return a.avg_val < b.avg_val; });
+	auto good_item = std::find_if_not(results.begin(), results.end(), [](const process_struct &a) { return a.avg_val <= 0; });
+	if (good_item == results.end())
+		goto NO_VALID;
+	std::cout << "\r\nSuccess ip address:" << std::endl;
+	std::for_each(good_item, results.end(),
+		[](const process_struct &a) { printf("ip address: %-15s, average time: %-6.2f\r\n", a.ip_addr.c_str(), a.avg_val); });
+	if (good_item != results.begin())
+	{
+		std::cout << "\r\nFailure ip address: " << std::endl;
+		std::for_each(results.begin(), good_item,
+			[](const process_struct &a) { std::cout << "ip address: " << a.ip_addr << std::endl;});
+	}
+	pause();
+	return 0;
 }
 
--- multithreading_ping/thread.h
+++ multithreading_ping/thread.h
@@ -2,92 +2,105 @@
 #include <vector>
 #include <fstream>
 #include <iostream>
-#include <stdlib.h>
-
+#include <stdlib.h> /* system, NULL, EXIT_FAILURE */
+#include <stdio.h>  /* popen */
 std::string ping(std::string);
 double get_avg(std::string);
 
-struct process_struct 
+void pause()
 {
-		std::string ip_addr;
-		double avg_val;
-		process_struct() = default;
-		process_struct(std::string str, double dval = 0.0): ip_addr(str), avg_val(dval) {}
+	int buffer;
+	printf("\r\nPress Enter key to continue...");
+	std::cin >> buffer;
+}
+
+struct process_struct
+{
+	std::string ip_addr;
+	double avg_val;
+	process_struct() = default;
+	process_struct(std::string str, double dval = 0.0) : ip_addr(str), avg_val(dval) {}
 };
 
 
 #ifdef _WIN32
-	#include <Windows.h>
-	DWORD WINAPI get_avg_for_ip_addr(LPVOID lpParameter)
+#include <Windows.h>
+#define POPEN _popen
+#define PCLOSE _pclose
+DWORD WINAPI get_avg_for_ip_addr(LPVOID lpParameter)
+{
+	process_struct *ip_val_ptr = static_cast<process_struct *>(lpParameter);
+	std::string line = ping(ip_val_ptr->ip_addr);
+	ip_val_ptr->avg_val = get_avg(line);
+	return 0;
+}
+std::vector<process_struct> main_func(std::ifstream &fin)
+{
+	std::vector<HANDLE> vec_threads;
+	std::vector<process_struct> vec_process_struct;
+	std::string ip_addr;
+	while (fin >> ip_addr)
 	{
-			process_struct *ip_val_ptr = static_cast<process_struct *>(lpParameter);
-			std::string line = ping(ip_val_ptr->ip_addr);
-			ip_val_ptr->avg_val = get_avg(line);
-			return 0;
+		process_struct new_val(ip_addr);
+		HANDLE thread;
+		vec_process_struct.push_back(ip_addr);
+		vec_threads.push_back(thread);
 	}
-	std::vector<process_struct> main_func(std::ifstream &fin)
+	for (std::vector<std::string>::size_type index = 0; index < vec_process_struct.size(); ++index)
 	{
-			std::vector<HANDLE> vec_threads;
-			std::vector<process_struct> vec_process_struct;
-			std::string ip_addr;
-			while (fin >> ip_addr)
-			{
-					process_struct new_val(ip_addr);
-					HANDLE thread;
-					vec_process_struct.push_back(ip_addr);
-					vec_threads.push_back(thread);
-			}
-			for (std::vector<std::string>::size_type index = 0; index < vec_process_struct.size(); ++index)
-			{
-					vec_threads[index] = CreateThread(NULL, 0, get_avg_for_ip_addr, &vec_process_struct[index], 0, 0);
-			}
-			for (const auto &thread : vec_threads)
-			{
-					WaitForSingleObject(thread, INFINITE);
-			}
+		vec_threads[index] = CreateThread(NULL, 0, get_avg_for_ip_addr, &vec_process_struct[index], 0, 0);
 	}
+	for (const auto &thread : vec_threads)
+	{
+		WaitForSingleObject(thread, INFINITE);
+	}
+	return vec_process_struct;
+}
 
 
-	
+
 
 #else
-	#include <pthread.h>
-	void *get_avg_for_ip_addr(void *ip_val_obj)
-	{
-			process_struct *ip_val_ptr = static_cast<process_struct *>(ip_val_obj);
-			std::string line = ping(ip_val_ptr->ip_addr);
-			ip_val_ptr->avg_val = get_avg(line);
-			pthread_exit(ip_val_obj);
-	}
-	std::vector<process_struct> main_func(std::ifstream &fin)
-	{
-			std::vector<pthread_t> vec_threads;
-			std::vector<process_struct> vec_process_struct;
+#include <pthread.h>
+#define POPEN popen
+#define PCLOSE pclose
+void *get_avg_for_ip_addr(void *ip_val_obj)
+{
+	process_struct *ip_val_ptr = static_cast<process_struct *>(ip_val_obj);
+	std::string line = ping(ip_val_ptr->ip_addr);
+	ip_val_ptr->avg_val = get_avg(line);
+	pthread_exit(ip_val_obj);
+}
+std::vector<process_struct> main_func(std::ifstream &fin)
+{
+	std::vector<pthread_t> vec_threads;
+	std::vector<process_struct> vec_process_struct;
 
-			std::string ip_addr;
-			int iret;
-			while (fin >> ip_addr)
-			{
-					process_struct new_val(ip_addr);
-					pthread_t thread;  // pthread_t ===> unsigned type, copy assign to push back
+	std::string ip_addr;
+	int iret;
+	while (fin >> ip_addr)
+	{
+		process_struct new_val(ip_addr);
+		pthread_t thread;  // pthread_t ===> unsigned type, copy assign to push back
 
-					vec_threads.push_back(thread);
-					vec_process_struct.push_back(new_val);
-			}
-			for (std::vector<std::string>::size_type index = 0; index < vec_process_struct.size(); ++index)
-			{
-					iret = pthread_create(&vec_threads[index], NULL, get_avg_for_ip_addr, (void *)&vec_process_struct[index]);
-					if (iret)
-					{
-							std::cerr << "pthread_create() for ip_addr: " << vec_process_struct[index].ip_addr << " return " << iret << std::endl;
-							exit(EXIT_FAILURE);
-					}
-			}
-			// wait for all thread
-			// each thread fill it's process_struct
-			for (const auto &thread : vec_threads)
-					pthread_join(thread, NULL);
-			return vec_process_struct;
+		vec_threads.push_back(thread);
+		vec_process_struct.push_back(new_val);
 	}
+	for (std::vector<std::string>::size_type index = 0; index < vec_process_struct.size(); ++index)
+	{
+		iret = pthread_create(&vec_threads[index], NULL, get_avg_for_ip_addr, (void *)&vec_process_struct[index]);
+		if (iret)
+		{
+			std::cerr << "pthread_create() for ip_addr: " << vec_process_struct[index].ip_addr << " return " << iret << std::endl;
+			pause();
+			exit(EXIT_FAILURE);
+		}
+	}
+	// wait for all thread
+	// each thread fill it's process_struct
+	for (const auto &thread : vec_threads)
+		pthread_join(thread, NULL);
+	return vec_process_struct;
+}
 
 #endif
